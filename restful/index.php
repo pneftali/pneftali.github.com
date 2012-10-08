1b40b2af05c828869c8ce81ba7b2ac84261a8e6b
Neftali Papelleras <papelleras.nef@gmail.com>
2012-10-08 19:46:01 +0800

    Alot of stuff added
    From two column layout to responsive to forms.

--- restful/index.php
+++ restful/index.php
@@ -2,7 +2,32 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel='stylesheet' href='./css/bootstrap.css' type='text/css' media='all' />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel='stylesheet' href='./css/bootstrap.min.css' type='text/css' media='all' />
+<link rel='stylesheet' href='./css/bootstrap-responsive.min.css' type='text/css' media='all' />
+<style>
+	a.add-more { font-size: 14px; color: #222; }
+	a.add-more:hover { cursor: pointer; color: #000; }
+	label { font-size: 12px; }
+
+	.inline { display: inline; }
+	.v-top { vertical-align: top !important; }
+
+	.bb-bot { margin-top: 20px; }
+
+
+	.navbar-inverse .navbar-inner {	
+		background-color: #0C8BEE;
+		background-image: -moz-linear-gradient(top,#222,#111);
+		background-image: -webkit-gradient(linear,0 0,0 100%,from(#222),to(#111));
+		background-image: -webkit-linear-gradient(top,#0741D6,#1D75AF);
+		background-image: -o-linear-gradient(top,#222,#111);
+		background-image: linear-gradient(to bottom,#222,#111);
+		background-repeat: repeat-x;
+		border-color: #0C8BEE;
+		filter: progid:dximagetransform.microsoft.gradient(startColorstr='#ff222222',endColorstr='#ff111111',GradientType=0);
+	}
+</style>
 <script type='text/javascript' src='http://code.jquery.com/jquery-1.8.2.min.js'></script>
 <script type='text/javascript' src='./js/bootstrap.min.js'></script>
 <?php if( isset($_POST['password']) ) : ?>
@@ -13,74 +38,313 @@
 </head>
 
 <body>
-  <div class="container">
+
+	<div class="navbar navbar-inverse navbar-fixed-top">
+      	<div class="navbar-inner">
+        	<div class="container">
+          		<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+		            <span class="icon-bar"></span>
+		            <span class="icon-bar"></span>
+		            <span class="icon-bar"></span>
+          		</a>
+          		<a class="brand" href="#">PAWD Secretary</a>          		
+        	</div>
+      </div>
+    </div>
+  	<div class="container" style="margin:100px auto;">
     
   	<div class="row">
+	  	<form class="form-horizontal">	    
+	    	<legend>Water District</legend>
+	    	<div class="span6">
+		      	<fieldset>		      	
+			      	<div class="control-group">
+			        	<label class="control-label" for="wd-1">Name</label>
+			        	<div class="controls">
+			          		<input type="text" class="input-xlarge" id="wd-1" name='wd-name' required placeHolder="John Doe">
+			        	</div>
+			      	</div>
+		      	</fieldset>
+		      	<fieldset>
+			      	<div class="control-group">
+			        	<label class="control-label" for="wd-2">Address</label>
+			        	<div class="controls">
+			          		<input type="text" class="input-xlarge" id="wd-2" name='wd-address' required placeHolder="jdoe">
+			        	</div>
+			      	</div>
+		      	</fieldset>
+		      	<fieldset>
+			      	<div class="control-group">
+			        	<label class="control-label" for="wd-3">Zip Code</label>
+			        	<div class="controls">
+			          		<input type="text" class="input-xlarge" id="wd-3" name='wd-zip' required placeHolder="">
+			        	</div>
+			      	</div>
+		      	</fieldset>
+		      	<fieldset>
+			      	<div class="control-group">
+			        	<label class="control-label" for="wd-4">Email</label>
+			        	<div class="controls">
+			          		<input type="email" class="input-xlarge" id="wd-4" name='wd-email' required placeHolder="">
+			        	</div>
+			      	</div>
+		      	</fieldset>
+		      	<fieldset>
+			      	<div class="control-group">
+			        	<label class="control-label" for="wd-5">Mobile</label>
+			        	<div class="controls">
+			          		<input type="text" class="input-xlarge" id="wd-5" name='wd-mobile' required placeHolder="">
+			        	</div>
+			      	</div>
+		      	</fieldset>
+		      	<fieldset>
+			      	<div class="control-group">
+			        	<label class="control-label" for="wd-6">Fax Number</label>
+			        	<div class="controls">
+			          		<input type="text" class="input-xlarge" id="wd-6" name='wd-fax' required placeHolder="">
+			        	</div>
+			      	</div>
+		      	</fieldset>
+	      	</div>
+	      	<div class="span6">
+		      	<fieldset>
+			      	<div class="control-group">
+			        	<label class="control-label" for="wd-7">CCC Number</label>
+			        	<div class="controls">
+			          		<input type="text" class="input-xlarge" id="wd-7" name='wd-ccc' required placeHolder="">
+			        	</div>
+			      	</div>
+		      	</fieldset>
+		      	<fieldset>
+			      	<div class="control-group">
+			        	<label class="control-label" for="wd-8">CCC Date of Issue</label>
+			        	<div class="controls">
+			          		<input type="text" class="input-xlarge" id="wd-8" name='wd-doi' required placeHolder="">
+			        	</div>
+			      	</div>
+		      	</fieldset>	      	
+		      	<fieldset>
+			      	<div class="control-group">
+			        	<label class="control-label" for="wd-9">LWUA Area</label>
+			        	<div class="controls">
+			          		<select class="" id="wd-9" name='wd-area' required>
+			          			<option value="">Please select</option>		          			
+			          		</select>	
+			        	</div>
+			      	</div>
+		      	</fieldset>
+		      	<fieldset>
+			      	<div class="control-group">
+			        	<label class="control-label" for="wd-10">Category</label>
+			        	<div class="controls">
+			          		<select class="" id="wd-10" name='wd-category' required>
+			          			<option value="">Please select</option>		          			
+			          		</select>	
+			        	</div>
+			      	</div>
+		      	</fieldset>
+		      	<fieldset>
+			      	<div class="control-group">
+			        	<label class="control-label" for="wd-11">No. Service Connections</label>
+			        	<div class="controls">
+			          		<input type="text" class="input-xlarge" id="wd-11" name='wd-asc' required placeHolder="">
+			        	</div>
+			      	</div>
+		      	</fieldset>
+		      	<fieldset>
+			      	<div class="control-group">
+			        	<label class="control-label" for="wd-12">City/Municipality</label>
+			        	<div class="controls">
+			          		<input type="text" class="input-xlarge" id="wd-12" name='wd-cm' required placeHolder="">
+			        	</div>
+			      	</div>
+		      	</fieldset>
+	      	</div>
+	      	
 
-	  <form method="POST" action="/" class="form-horizontal">
-	    <div class="span6">
-	    	<legend>Registration</legend>
-	      	<fieldset>		      	
-		      	<div class="control-group">
-		        	<label class="control-label" for="input01">Fullname</label>
-		        	<div class="controls">
-		          		<input type="text" class="input-xlarge" id="input01" required placeHolder="John Doe">
-		        	</div>
-		      	</div>
-	      	</fieldset>
-	      	<fieldset>
-		      	<div class="control-group">
-		        	<label class="control-label" for="input02">Username</label>
-		        	<div class="controls">
-		          		<input type="text" class="input-xlarge" id="input02" required placeHolder="jdoe">
-		        	</div>
-		      	</div>
-	      	</fieldset>
-	      	<fieldset>
-		      	<div class="control-group">
-		        	<label class="control-label" for="input03">Email</label>
-		        	<div class="controls">
-		          		<input type="email" class="input-xlarge" id="input03" required placeHolder="john.doe@gmail.com">
-		        	</div>
-		      	</div>
-	      	</fieldset>
-	      	<fieldset>
-		      	<div class="control-group">
-		        	<label class="control-label" for="input04">Password</label>
-		        	<div class="controls">
-		          		<input type="password" class="input-xlarge" id="input04" required placeHolder="">
-		        	</div>
-		      	</div>
-	      	</fieldset>
+	      	<legend>General Manager</legend>
+	      	<div class="span6">
+		      	<fieldset>
+			      	<div class="control-group">
+			        	<label class="control-label" for="gm-1">Name</label>
+			        	<div class="controls">
+			          		<input type="text" class="input-xlarge" id="gm-1" name='gm-name' required placeHolder="">
+			        	</div>
+			      	</div>
+		      	</fieldset>
+		      	<fieldset>
+			      	<div class="control-group">
+			        	<label class="control-label" for="gm-2">Date of Appointment</label>
+			        	<div class="controls">
+			          		<input type="text" class="input-xlarge" id="gm-2" name='gm-doa' required placeHolder="">
+			        	</div>
+			      	</div>
+		      	</fieldset>
+		      	<fieldset>
+			      	<div class="control-group">
+			        	<label class="control-label" for="gm-3">Profession</label>
+			        	<div class="controls">
+			          		<input type="text" class="input-xlarge" id="gm-3" name='gm-profession' required placeHolder="">
+			        	</div>
+			      	</div>
+		      	</fieldset>
+		      	<fieldset>
+			      	<div class="control-group">
+			        	<label class="control-label" for="gm-4">Date of Birth</label>
+			        	<div class="controls">
+			          		<input type="text" class="input-xlarge" id="gm-4" name='gm-dob' required placeHolder="">
+			        	</div>
+			      	</div>
+		      	</fieldset>
+		    </div>
+		    <div class="span6">  	
+		      	<fieldset>
+			      	<div class="control-group">
+			        	<label class="control-label">Gender</label>
+			        	<div class="controls">
+			        		<label class="radio inline">
+								<input type="radio" name="gm-gender" value='male' />
+								Male							  
+							</label>
+							<label class="radio inline">
+								<input type="radio" name="gm-gender" value='female' />
+								Female							  
+							</label>			          		
+			        	</div>
+			      	</div>
+		      	</fieldset>
+		      	<fieldset>
+			      	<div class="control-group">
+			        	<label class="control-label" for="gm-5">Mobile</label>
+			        	<div class="controls">
+			          		<input type="text" class="input-xlarge" id="gm-5" name='gm-mobile' required placeHolder="">
+			        	</div>
+			      	</div>
+		      	</fieldset>
+		      	<fieldset>
+			      	<div class="control-group">
+			        	<label class="control-label" for="gm-6">Advisor</label>
+			        	<div class="controls">
+			          		<input type="text" class="input-xlarge" id="gm-6" name='gm-advisor' required placeHolder="">
+			        	</div>
+			      	</div>
+		      	</fieldset>
+		      	<fieldset>
+			      	<div class="control-group">
+			        	<label class="control-label" for="gm-7">Designation</label>
+			        	<div class="controls">
+			          		<input type="text" class="input-xlarge" id="gm-7" name='gm-designation' required placeHolder="">
+			        	</div>
+			      	</div>
+		      	</fieldset>
+		    </div>  	
 
-	      	<legend>Registration</legend>
-	      	<fieldset>
-		      	<div class="control-group">
-		        	<label class="control-label" for="input05">Group</label>
-		        	<div class="controls">
-		          		<select class="" id="input05" required>
-		          			<option>Board of Director</option>
-		          			<option>Manager</option>
-		          		</select>	
-		        	</div>
-		      	</div>
-	      	</fieldset>
-	      	<fieldset>
-		      	<div class="control-group">
-		        	<label class="control-label" for="input06">Region</label>
-		        	<div class="controls">
-		          		<select class="" id="input06" required>
-		          			<option>Dipolog</option>
-		          			<option>Davao</option>
-		          			<option>Cebu</option>
-		          		</select>	
-		        	</div>
-		      	</div>
-	      	</fieldset>
+	      	<legend>
+	      		Board of Directors
+	      		<a class='add-more pull-right'>
+	      			<i class="icon-plus-sign"></i>&nbsp;Add more
+	      		</a>
+	      	</legend>	      	
+	      	<div id='bod-box'>
+	      		<div id='clone-this'>
+		      		<div class="span6">
+				      	<fieldset>
+					      	<div class="control-group">
+					        	<label class="control-label" for="bb-1-1">Sector</label>
+					        	<div class="controls">
+					          		<select class="" id="bb-1-1" name='bb-sector' required>
+					          			<option>Educational</option>
+					          			<option>Professional</option>
+					          			<option>Civic-Oriented</option>
+					          			<option>Business</option>
+					          			<option>Womens</option>
+					          			<option>LWUA 6th Member</option>
+					          		</select>	
+					        	</div>
+					      	</div>
+				      	</fieldset>
+				      	<fieldset>
+					      	<div class="control-group">
+					        	<label class="control-label" for="bb-1-2">Name</label>
+					        	<div class="controls">
+					          		<input type="text" class="input-xlarge" id="bb-1-2" name='bb-name' required placeHolder="">
+					        	</div>
+					      	</div>
+				      	</fieldset>
+				      	<fieldset>
+					      	<div class="control-group">
+					        	<label class="control-label" for="bb-1-3">Profession</label>
+					        	<div class="controls">
+					          		<input type="text" class="input-xlarge" id="bb-1-3" name='bb-profession' required placeHolder="">
+					        	</div>
+					      	</div>
+				      	</fieldset>
+				      	<fieldset>
+					      	<div class="control-group">
+					        	<label class="control-label" for="bb-1-4">Date of Birth</label>
+					        	<div class="controls">
+					          		<input type="text" class="input-xlarge" id="bb-1-4" name='bb-dob' required placeHolder="">
+					        	</div>
+					      	</div>
+				      	</fieldset>
+			      	</div>
+			      	<div class="span6">
+				      	<fieldset>
+					      	<div class="control-group">
+					        	<label class="control-label">Gender</label>
+					        	<div class="controls">
+					        		<label class="radio inline">
+										<input type="radio" name="gender" id="g-m" value='male' />
+										Male							  
+									</label>
+									<label class="radio inline">
+										<input type="radio" name="gender" id="g-f" value='female' />
+										Female							  
+									</label>			          		
+					        	</div>
+					      	</div>
+				      	</fieldset>
+				      	<fieldset>
+					      	<div class="control-group">
+					        	<label class="control-label" for="bb-1-5">Date of Appointment</label>
+					        	<div class="controls">
+					          		<input type="text" class="input-xlarge" id="bb-1-5" name='bb-doa' required placeHolder="">
+					        	</div>
+					      	</div>
+				      	</fieldset>
+				      	<fieldset>
+					      	<div class="control-group">
+					        	<label class="control-label" for="bb-1-6">Start Date</label>
+					        	<div class="controls">
+					          		<input type="text" class="input-xlarge" id="bb-1-6" name='bb-sdate' required placeHolder="">
+					        	</div>
+					      	</div>
+				      	</fieldset>
+				      	<fieldset>
+					      	<div class="control-group">
+					        	<label class="control-label" for="bb-1-7">End Date</label>
+					        	<div class="controls">
+					          		<input type="text" class="input-xlarge" id="bb-1-7" name='bb-edate' required placeHolder="">
+					        	</div>
+					      	</div>
+				      	</fieldset>
+				    </div>  				      	
+			    </div><!-- end clone-this -->
 
-	      	<input type='submit' id='reg-btn' value='Register' class="btn btn-large btn-primary pull-right" />
-	    </div>	    
-	  </form>
+			    <div class='clearfix'></div>
+			    <div class='bb-bot' id="bb-bot-1"></div>
+
+			</div>
+			<a class='add-more pull-right'>
+      			<i class="icon-plus-sign"></i>&nbsp;Add more
+      		</a>
+      		<div class='clearfix'></div>
+			<p class="pull-right" style="margin-top:50px;">
+			  	<button class="btn btn-large" type="button"><i class="icon-remove"></i>&nbsp;Cancel</button>
+	      		<input type='submit' id='reg-btn' value='Register' class="btn btn-large btn-primary" />
+			</p>			
+	    	    
+	  	</form>
 
 
 	  	<!-- modal for secretary -->
@@ -117,7 +381,7 @@
   			</form>				
 		</div>
 
-	</div>
+	</div><!-- end row -->
 
   </div>
 </body>
--- restful/js/app.js
+++ restful/js/app.js
@@ -1,5 +1,8 @@
 (function ($){
 
+	var top = 0,
+		bot = 2;
+
 	/* 
 	 * just a sample illustration of how to use RESTful request to the server
 	list();
@@ -15,15 +18,13 @@
 	}
 	*/
 
-
-	if (typeof pass === "undefined"){
-		$('#myModal').modal('show');		
-	} else{
+	$('#myModal').modal('show');
+	if (typeof pass !== "undefined"){
 		$('#myModal').modal('hide');
-		console.log('halo');
-	}	
+	} 	
 	
-	$('#myModal').on('hide', function (){
+	/*
+	$('#myModal').on('hidden', function (){
 
 		if (typeof pass === "undefined"){
 			// similar behavior as an HTTP redirect
@@ -31,6 +32,31 @@
 		} 
 		
 	});	
-		
+	*/
+
+
+	$( '.add-more' ).on('click', function (){
+		var addContent = 	"<div class='clearfix'></div>";
+			addContent +=	"<div class='bb-bot' id='bb-bot-"+bot+"'></div>";
+		$( '#clone-this' ).clone().appendTo( '#bod-box' );
+
+		top++;
+		bot++;
+		console.log(top);
+
+		$('html, body').animate({
+			scrollTop: $( "#bb-bot-"+top ).offset().top
+		}, 2000);
+
+		$( '#bod-box' ).append(addContent);
+
+	});
+
+
+	$('form').submit(function() {
+  		console.log($(this).serialize());
+  		return false;
+	});
+	
 
 })(jQuery);
